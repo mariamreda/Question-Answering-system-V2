161c70f1dddb59727ec2417c8289c6178c4fb5ec
Mariam Reda <mariamreda2911@gmail.com>
2024-05-27 15:08:30 +0200

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -41,7 +41,7 @@ def get_vectorstore(text_chunks):
 
 def get_conversation_chain(vectorstore):
 
-    llm = ChatOpenAI()
+    llm = ChatOpenAI(model_name="gpt-3.5-turbo-16k")
 
     memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
     conversation_chain = ConversationalRetrievalChain.from_llm(
